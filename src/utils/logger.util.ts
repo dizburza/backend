1262a5be28d46c180d1201c124e3c4e2a6a7ab6b
pope-h <hamatthan@gmail.com>
2025-10-14 16:24:58 +0100

    added logger file

---
+++ src/utils/logger.util.ts
@@ -0,0 +1,30 @@
+import winston from "winston";
+import { ENV } from "../config/environment";
+
+const logger = winston.createLogger({
+  level: ENV.NODE_ENV === "production" ? "info" : "debug",
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.errors({ stack: true }),
+    winston.format.splat(),
+    winston.format.json()
+  ),
+  defaultMeta: { service: "payroll-backend" },
+  transports: [
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+    new winston.transports.File({ filename: "logs/combined.log" }),
+  ],
+});
+
+if (ENV.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.simple()
+      ),
+    })
+  );
+}
+
+export default logger;
