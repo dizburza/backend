9a15bbdb3fc2dc6487fc34067a4c92d739e1afb8
pope-h <hamatthan@gmail.com>
2025-10-14 23:08:31 +0100

    implement security middleware

---
+++ src/middlewares/security.middleware.ts
@@ -0,0 +1,17 @@
+import helmet from "helmet";
+import hpp from "hpp";
+
+// Helmet for security headers
+export const helmetMiddleware = helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrc: ["'self'"],
+      imgSrc: ["'self'", "data:", "https:"],
+    },
+  },
+});
+
+// Prevent HTTP Parameter Pollution
+export const hppMiddleware = hpp();
